7f315ccdd9b2b5ffde58158a8c666ac23d1b15c9
BiporshiChakma <124572288+BiporshiChakma@users.noreply.github.com>
2023-02-14 12:01:44 +0600

    Update live test.php

--- live test.php
+++ live test.php
@@ -19,17 +19,3 @@ function sum($x,int...$n){
     }
 }
 sum(8,5,7,1,2,8,4,3);  
-/*
-
-$nums = [5,7,1,2,8,4,3];
-$target = 8;
-function twoSum( $nums, $target ) {
-    for ( $i = 0; $i < count( $nums ); $i++ ) {
-        for ( $j = $i + 1; $j < count( $nums ); $j++ ) {
-            if ( $nums[$i] + $nums[$j] == $target ) {
-                return [$i, $j];
-            }
-        }
-    }
-}
-print_r( twoSum( $nums, $target ) );*/
